237c39104e52b893915b409cbb5fca287e97c02a
Sam Gentle <sam@samgentle.com>
2016-09-08 23:10:08 +1000

    Add schema validation to root reducer

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import createValidatedReducer from 'redux-json-schema'
 import App from './containers/App'
 import reducer from './reducers'
+import schemas from '../schemas'
 import 'todomvc-app-css/index.css'
 
-const store = createStore(reducer)
+const store = createStore(createValidatedReducer(reducer, schemas.app, { schemas }))
 
 render(
   <Provider store={store}>
